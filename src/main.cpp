71496bab7b447906204a74833ca46330572e8c2e
Shaw <sunxycn98@gmail.com>
2022-05-29 17:56:36 -0400

    main function done

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 bool game_over;
 std::string answer;
 std::string current_guess="";
+std::string guess_buffer="";
 int answer_length;
 int num_attempt = 7;
 int height = 16;
@@ -12,58 +13,95 @@ int width = 27;
 
 void PrintMessage();
 void GetInput();
-void CheckAnswer();
+void CheckAnswer(char);
+void UpdateGuessBuffer();
+void InitializeGuessBuffer();
 
 int main(){
-    std::cout << "Please input the answer word: ";
+    std::cout << "Please input the answer word: (q to quit) ";
     std::cin >> answer;
+    if (answer == "q"){
+        return 0;
+    }
     answer_length = answer.length();
-    std::cout << "Game will start in 3 seconds..." << std::endl;
-    sleep(3);
+    std::cout << "Game will start in 3..." << std::endl;
+    sleep(1);
+    std::cout << "Game will start in 2..." << std::endl;
+    sleep(1);
+    std::cout << "Game will start in 1..." << std::endl;
+    sleep(1);
     system("clear");
-    std::cout << "Game Start!" << std::endl;
+
     game_over = false;
+
     while (!game_over){
+        if (num_attempt == 0){
+            std::cout << "You run out of guesses!" << std::endl;
+            std::cout << "Want to know the answer? (T/F) ";
+            char show_answer;
+            std::cin >> show_answer;
+            if (show_answer == 'T'){
+            std::cout << "The answer is: " << answer << std::endl;
+            }
+            return 0;
+        }
+        system("clear");
+        if (guess_buffer == ""){
+            InitializeGuessBuffer(); 
+        }
+           
         PrintMessage();
         GetInput();
-        CheckAnswer();
+        UpdateGuessBuffer();  
     }
+
     return 0;
 }
 
-void CheckAnswer(){
-    if (current_guess == answer){
-        game_over = true;   
+void CheckAnswer(char letter){
+    /* If has full answer, set gane_over to true
+       If no full answer yet:
+            If current letter is wrong, attempt decrement
+            If current letter is right, append to current_guess*/
+    char answer_letter = answer[current_guess.length()];
+    if (answer_letter == letter) {
+        current_guess += letter;
+    } else{
+        num_attempt -= 1;
     }
+    if (current_guess == answer){
+        system("clear");
+        UpdateGuessBuffer(); 
+        PrintMessage();
+        std::cout << "You got it!" << std::endl;
+        game_over = true;   
+    } 
 }
 
 void GetInput(){
-    std::string current_letter;
+    /* get current input letter; attempt number decrement */ 
+    char current_letter;
     std::cout << "Please input one letter: ";
     std::cin >> current_letter;
-    current_guess += current_letter;
-    num_attempt -= 1;
-    //system("clear");
+    CheckAnswer(current_letter);
+}
+
+void InitializeGuessBuffer(){  
+    for (int i = 0; i < width-9; i++){
+        if (i < answer_length){
+            guess_buffer += "_";
+        } else {
+            guess_buffer += " ";
+        }
+    } 
+}
+
+void UpdateGuessBuffer(){
+    std::string rear = guess_buffer.substr(current_guess.length());
+    guess_buffer = current_guess + rear;
 }
 
 void PrintMessage(){
-    std::string guess_buffer = current_guess;
-    int underscore_len = answer_length-current_guess.length();
-    for (int i = 0; i < width-9; i++){
-        if (current_guess.length() == 0){   // when no guess, print "Guess: ____"
-            if (i < answer_length){
-                guess_buffer += "_";
-            } else {
-                guess_buffer += " ";
-            }
-        } else {                            // when exist guess, print "Guess: xx___"
-            if (i >= current_guess.length() && i < current_guess.length()+ underscore_len){
-                guess_buffer += "_";
-            } else if (i >= current_guess.length()+ underscore_len){
-                guess_buffer += " ";
-            }
-        }
-    }
     for (int i = 0; i < height; i++){
         if (i == 0 || i == 2 || i == 15){
             std::cout << "+=========================+" << std::endl;
@@ -85,7 +123,7 @@ void PrintMessage(){
 
 /*
 +=========================+0    width = 27
-|      Hangman Game       |1    height = 15
+|      Hangman Game       |1    height = 16
 +=========================|2
 |                         |3
 |      +-----------       |4
